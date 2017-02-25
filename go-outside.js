0dedc83bbf4011075bd48bcd127101a33302e5df
vagrant <vagrant@lighthouse-labs>
2017-02-25 05:52:30 +0000

    Adds additional exercises

---
+++ go-outside.js
@@ -0,0 +1,11 @@
+var temperature = 12;
+
+if (temperature > 0) {
+  console.log("Wear a scarf!");
+} else if (temperature < 15) {
+  console.log("Short sleeves no way.");
+} else {
+  console.log("Short sleeves okay.");
+}
+
+console.log("Now get outside!");
